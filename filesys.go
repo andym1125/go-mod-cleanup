7d1520764a8459db3d417351235be87b624c8abb
Andy <andy@Andys-MBP.lan>
2022-07-15 12:16:49 -0500

    Iterates through a given directory and uses Go compiler functions to get imports

--- filesys.go
+++ filesys.go
@@ -1,31 +1,64 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"go/parser"
+	"go/token"
 	"os"
+	"regexp"
 )
 
+var importRegex *regexp.Regexp
+var importsRegex *regexp.Regexp
+var nativeImportRegex *regexp.Regexp
+var githubImportRegex *regexp.Regexp
+var stripQuotesRegex *regexp.Regexp
+
+func init() {
+	importRegex = regexp.MustCompile(`import\s*\(\s*(?:[^"]+"([^"]+)"\s*)+\)`)
+	importsRegex = regexp.MustCompile(`"[^"]+"`)
+	nativeImportRegex = regexp.MustCompile(`^[^\/]+$`)
+	githubImportRegex = regexp.MustCompile(`github.com\/[^\/]+\/[^\/]+`)
+	stripQuotesRegex = regexp.MustCompile(`[^"]+`)
+}
+
+func ParseImports(file *os.File) []string {
+	parsedFile, err := parser.ParseFile(token.NewFileSet(), file.Name(), nil, parser.ImportsOnly)
+	if err != nil {
+		panic(err)
+	}
+
+	ret := make([]string, 0)
+	for _, a := range parsedFile.Imports {
+		ret = append(ret, a.Path.Value)
+	}
+
+	return ret
+}
+
 func DependencySearch(rootDir string) {
-	file, err := os.Open(rootDir)
-	if err != nil {
-		panic(err)
-	}
+	output := RecursiveTouch(rootDir, func(file *os.File) string {
+		ret, err := json.Marshal(ParseImports(file))
+		if err != nil {
+			panic(err)
+		}
 
-	filestat, err := file.Stat()
-	if err != nil {
-		panic(err)
-	}
+		return string(ret)
+	})
 
-	if !filestat.IsDir() {
-		//SkimDependency(file)
-	} else {
-
+	for _, arrstr := range output {
+		var arr *[]string
+		json.Unmarshal([]byte(arrstr), arr)
+		fmt.Println(arr)
 	}
 }
 
 /* RecursiveTouch iterates through all the files rootDir recursively, executing the callback on
-each non-directory file. */
-func RecursiveTouch(rootDir string, callback func(file *os.File)) {
+each non-directory file. Callback allows for a string return which allows data to be transferred
+out of the function. */
+func RecursiveTouch(rootDir string, callback func(file *os.File) string) []string {
+	var data []string
 	file, err := os.Open(rootDir)
 	if err != nil {
 		panic(err)
@@ -43,10 +76,41 @@ func RecursiveTouch(rootDir string, callback func(file *os.File)) {
 		}
 
 		for _, d := range dirs {
-			fmt.Println(d.Name())
-			RecursiveTouch(rootDir+string(os.PathSeparator)+d.Name(), callback)
+			data = append(data, RecursiveTouch(rootDir+string(os.PathSeparator)+d.Name(), callback)...)
 		}
 	} else {
-		callback(file)
+		data = append(data, callback(file))
 	}
+
+	return data
+}
+
+/* ScrapeImports takes in an import string and adds it to the Set that was passed in. If set is
+nil, a Set will be created for use only internally. */
+func ScrapeImports(input string, set *Set[string]) {
+	if set == nil {
+		set = NewSet[string]()
+	}
+
+	importStatement := importRegex.FindString(input)
+	imports := importsRegex.FindAllString(importStatement, -1)
+	for _, a := range imports {
+		if strippedImport := StripImport(a); strippedImport != "" {
+			set.Add(strippedImport)
+		}
+	}
+}
+
+/* StripImport strips an import string down so it can be better compared to a dependency string.
+For native packages (ie "errors"), none of the import will be preserved.
+For github imports, only "github.com", the username, and the repo name will be preserved.
+For all other imports, the entire string will be preserved. */
+func StripImport(input string) string {
+	if output := nativeImportRegex.FindString(input); output != "" {
+		return ""
+	} else if output := githubImportRegex.FindString(input); output != "" {
+		return stripQuotesRegex.FindString(output)
+	}
+
+	return stripQuotesRegex.FindString(input)
 }
--- filesys_test.go
+++ filesys_test.go
@@ -3,13 +3,54 @@ package main
 import (
 	"fmt"
 	"os"
+	"sort"
 	"testing"
 )
 
-func TestSkim(t *testing.T) {
+func TestRecursiveTouch(t *testing.T) {
 
-	RecursiveTouch("../sonr/internal/motor", func(file *os.File) {
+	strs := RecursiveTouch("../sonr/internal/motor", func(file *os.File) string {
 		filestat, _ := file.Stat()
-		fmt.Println("==" + filestat.Name())
+		return "==" + filestat.Name()
 	})
+
+	fmt.Println(strs)
+}
+
+func TestParseImports(t *testing.T) {
+	file, err := os.Open("/Users/andy/Documents/workshop/sonr/internal/motor/motor.go")
+	if err != nil {
+		panic(err)
+	}
+	ParseImports(file)
+}
+
+func TestDependencySearch(t *testing.T) {
+	DependencySearch("/Users/andy/Documents/workshop/sonr/internal/motor")
+}
+
+func TestScrapeImports(t *testing.T) {
+	set := NewSet[string]()
+
+	expected := []string{"github.com/kataras/golog", "github.com/libp2p/go-libp2p-core",
+		"github.com/sonr-io/sonr", "github.com/kataroas/golog", "random.com/sonr-io/sonr/pkg/fs"}
+	sort.Strings(expected)
+
+	ScrapeImports(`import ( "errors"  "github.com/kataras/golog" "github.com/libp2p/go-libp2p-core/protocol" device "github.com/sonr-io/sonr/pkg/fs")`, set)
+	ScrapeImports(`import ( "errors"  "github.com/kataroas/golog" "github.com/libp2p/go-libp2p-core/hey" device "random.com/sonr-io/sonr/pkg/fs")`, set)
+	ScrapeImports(`import ( "errors"  "github.com/kataras/golog" "github.com/libp2p/go-libp2p-core/protocol" device "github.com/sonr-io/sonr/pkg/fs")`, set)
+	actual := set.ToArray()
+	sort.Strings(actual)
+
+	fmt.Println(actual)
+	fmt.Println(expected)
+
+	if len(actual) != len(expected) {
+		t.FailNow()
+	}
+	for i := 0; i < len(actual); i++ {
+		if actual[i] != expected[i] {
+			t.FailNow()
+		}
+	}
 }
